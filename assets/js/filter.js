c4d5e33d776861e93b7f7f6bff74594b30d45a90
ckfx1995 <ckfx1995@gmail.com>
2017-07-24 14:49:51 +0800

    new JS Function

---
+++ assets/js/filter.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+                  $("#new").click(function(){
+                                    $(".el").css("display","none");
+                                    $(".new").css("display","");
+                                    });
+                  $("#all").click(function(){
+                                  $(".el").css("display","");
+                                  });
+                  $("#type1").click(function(){
+                                  $(".el").css("display","none");
+                                  $(".type1").css("display","");
+                                  });
+                  });
