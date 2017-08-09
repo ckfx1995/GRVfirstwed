67e29c53dec7b9bf3564078db47b52bb5c6a17a9
ckfx1995 <ckfx1995@gmail.com>
2017-08-09 11:42:10 +0800

    homepage animate and about with numbering

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
                   $(window).scroll( function(){
                                    /* Check the location of each desired element */
                                    $('.hideme').each( function(i){
-                                                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                                                     var bottom_of_object = $(this).offset().top + ($(this).outerHeight()/2);
                                                      var bottom_of_window = $(window).scrollTop() + $(window).height();
                                                      /* If the object is completely visible in the window, fade it it */
                                                      if( bottom_of_window > bottom_of_object ){
@@ -27,7 +27,7 @@ $(document).ready(function() {
                   $(window).scroll( function(){
                                    /* Check the location of each desired element */
                                    $('.hideme2').each( function(i){
-                                                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                                                     var bottom_of_object = $(this).offset().top + ($(this).outerHeight()/2);
                                                      var bottom_of_window = $(window).scrollTop() + $(window).height();
                                                      /* If the object is completely visible in the window, fade it it */
                                                      if( bottom_of_window > bottom_of_object ){
@@ -52,7 +52,7 @@ $(document).ready(function() {
                                    /* Check the location of each desired element */
                                    $('.hideme3').each( function(i){
                                                       var helper=0;
-                                                      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                                                      var bottom_of_object = $(this).offset().top + ($(this).outerHeight()/2);
                                                       var bottom_of_window = $(window).scrollTop() + $(window).height();
                                                       /* If the object is completely visible in the window, fade it it */
                                                       if( bottom_of_window > bottom_of_object  ){
@@ -77,7 +77,7 @@ $(document).ready(function() {
                                    /* Check the location of each desired element */
                                    $('.RWDr').each( function(i){
                                                       var helper=0;
-                                                      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                                                      var bottom_of_object = $(this).offset().top + ($(this).outerHeight()/2);
                                                       var bottom_of_window = $(window).scrollTop() + $(window).height();
                                                       /* If the object is completely visible in the window, fade it it */
                                                       if( bottom_of_window > bottom_of_object  ){
